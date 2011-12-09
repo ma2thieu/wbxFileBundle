bdab821822cdf59fef6f157a0a7abf9825971f13
Matthieu Robillard <m@tthi.eu>
2011-12-09 18:03:49 +0100

    fixed wrong web path

--- Entity/File.php
+++ Entity/File.php
@@ -186,7 +186,7 @@ class File {
     }
 
     protected function getUploadRootDir() {
-        return __DIR__ . '/../../../../web/' . $this->getUploadDir();
+        return __DIR__ . '/../../../../../web/' . $this->getUploadDir();
     }
 
     protected function getUploadDir() {
