7610971247a097808a81ecbe3a0112098de4b980
Matthieu Robillard <m@tthi.eu>
2012-06-19 17:25:02 +0200

    add an optional checkbox to remove the uploaded file

--- Form/FileType.php
+++ Form/FileType.php
@@ -6,12 +6,25 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilder;
 
 class FileType extends AbstractType {
+	
+	protected $with_empty;
+    
+	public function __construct($with_empty = false) {
+	    $this->with_empty = $with_empty;
+	}
 
     public function buildForm(FormBuilder $builder, array $options) {
         $builder
             ->add('name', 'text', array('required' => false))
             ->add('file')
             ->add('is_file_changed', 'hidden');
+
+		if ($this->with_empty) {
+		    $builder->add('to_empty', 'checkbox', array(
+		        'label' => 'Empty',
+		        'required' => false,
+		    ));
+		}
     }
 
     public function getName() {
