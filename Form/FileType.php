ab63c09beab6c978e1d0f72a3c323d0878a057e8
Matthieu Robillard <m@tthi.eu>
2013-05-30 09:55:47 +0200

    add option to show/hide the "name" field

--- Form/FileType.php
+++ Form/FileType.php
@@ -9,21 +9,32 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 class FileType extends AbstractType {
 
     protected $with_empty;
+    protected $with_name;
 
-    public function __construct($with_empty = false) {
+    public function __construct($with_empty = false, $with_name = false) {
         $this->with_empty = $with_empty;
+        $this->with_name = $with_name;
     }
 
     public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
-            ->add('name', 'text', array('required' => false))
-            ->add('file', 'file', array('required' => false))
+            ->add('file', 'file', array(
+                'required'  => false,
+                'label'     => 'wbxfilebundle.form.file'
+            ))
             ->add('is_file_changed', 'hidden');
+
+        if ($this->with_name) {
+            $builder->add('name', 'text', array(
+                'required'  => false,
+                'label'     => 'wbxfilebundle.form.name'
+            ));
+        }
 
         if ($this->with_empty) {
             $builder->add('to_empty', 'checkbox', array(
-                'label'     => 'Empty',
                 'required'  => false,
+                'label'     => 'wbxfilebundle.form.empty'
             ));
         }
     }
